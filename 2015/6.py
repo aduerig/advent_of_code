87b66e2831547fc3e4ece47dfd4a454e92db4427
Andrew Duerig <duerig.andrew@gmail.com>
2023-12-07 12:51:25 -0500

    ok

--- 2015/6.py
+++ 2015/6.py
@@ -1,3 +1,4 @@
+
 # https://adventofcode.com/2023
 import sys
 import pathlib
@@ -18,17 +19,68 @@ with open(data_file) as f:
 
             before, last = line.split('through')
             x2, y2 = list(map(int, last.strip().split(','))) 
-            on_off, first = before.strip().split('')
+            on_off, first = before.strip().split()
             x1, y1 = list(map(int, first.strip().split(',')))
-            on_off = on_off.strip()
-            instructions.append(on_off, [x1, y1], [x2, y2])
-        instructions.append(line)
+            on_off_toggle = on_off.strip()
+            instructions.append((on_off, [x1, y1], [x2, y2]))
 
+val = {
+    'toggle': 2,
+    'on': 1,
+    'off': -1,
+}
 
 grid = [[0 for _ in range(1000)] for _ in range(1000)]
-for on_off, (x1, y1), (x2, y2) in instructions:
+for on_off_toggle, (x1, y1), (x2, y2) in instructions:
     if x2 < x1:
-        print_red('ok')
-        exit()
-    for 
-    pass
+        print_red('ok') or exit()
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            grid[y][x] = max(0, grid[y][x] + val[on_off_toggle])
+
+print(sum([sum(x) for x in grid]))
+
+# part 1
+# # https://adventofcode.com/2023
+# import sys
+# import pathlib
+
+# filepath = pathlib.Path(__file__)
+# sys.path.append(str(filepath.parent.parent))
+# from helpers import * 
+
+# filepath = pathlib.Path(__file__)
+# data_file = filepath.parent.joinpath(filepath.stem + '.dat')
+
+# instructions = []
+# with open(data_file) as f:
+#     for line in f.readlines():
+#         line = line.strip()
+#         if line:
+#             line = line.replace('turn ', '')
+
+#             before, last = line.split('through')
+#             x2, y2 = list(map(int, last.strip().split(','))) 
+#             on_off, first = before.strip().split()
+#             x1, y1 = list(map(int, first.strip().split(',')))
+#             on_off_toggle = on_off.strip()
+#             instructions.append((on_off, [x1, y1], [x2, y2]))
+
+# val = {
+#     'on': 1,
+#     'off': 0,
+# }
+
+
+# grid = [[0 for _ in range(1000)] for _ in range(1000)]
+# for on_off_toggle, (x1, y1), (x2, y2) in instructions:
+#     if x2 < x1:
+#         print_red('ok') or exit()
+#     for x in range(x1, x2 + 1):
+#         for y in range(y1, y2 + 1):
+#             if on_off_toggle == 'toggle':
+#                 grid[y][x] = 1 - grid[y][x]
+#             else:
+#                 grid[y][x] += val[on_off_toggle]
+
+# print(sum([x.count(1) for x in grid]))
--- 2015/7.py
+++ 2015/7.py
@@ -9,7 +9,58 @@ from helpers import *
 filepath = pathlib.Path(__file__)
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
+
+funcs = {
+    'AND': lambda x, y: x & y,
+    'LSHIFT': lambda x, y: x << y,
+    'RSHIFT': lambda x, y: x >> y,
+    'OR': lambda x, y: x | y,
+}
+
+
+def get_val(s):
+    if s in thing:
+        return thing[s]
+    return int(s)
+
+thing = {}
+instructions = {}
 with open(data_file) as f:
     for line in f.readlines():
         line = line.strip()
+        ops, assignee = line.split('->')
         
+        ops = ops.strip()
+        assignee = assignee.strip()
+
+        dependancies = [x for x in ops.split() if x.islower()]
+        instructions[assignee] = [ops, dependancies]
+
+
+visited = set()
+order = []
+def dfs(node):
+    if node in visited:
+        return
+    visited.add(node)
+    for d in instructions[node][1]:
+        dfs(d)
+    order.append(node)
+dfs('a')
+
+for assignee in order:
+    ops = instructions[assignee][0]
+    if 'NOT' in ops:
+        thing[assignee] = ~thing[ops.replace('NOT ', '')]
+    else:
+        for key, func in funcs.items():
+            if key in ops:
+                l, r = ops.split(f' {key} ')
+                val = func(get_val(l), get_val(r))
+                break
+        else:
+            val = get_val(ops)
+        thing[assignee] = val
+
+
+print(thing['a'])
