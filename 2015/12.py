50da80a1684ee0de745d273789975eccf297793d
Andrew Duerig <duerig.andrew@gmail.com>
2023-12-07 18:48:42 -0500

    ok

--- 2015/12.py
+++ 2015/12.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2023
 import sys
 import pathlib
+import re
 
 filepath = pathlib.Path(__file__)
 sys.path.append(str(filepath.parent.parent))
@@ -9,7 +10,41 @@ from helpers import *
 filepath = pathlib.Path(__file__)
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
+
+def dfs(d):
+    if type(d) == int:
+        return d
+    elif type(d) == list:
+        return sum([dfs(s) for s in d])
+    elif type(d) == dict:
+        if 'red' in d or 'red' in d.values():
+            return 0
+        return sum([dfs(s) for s in d.values()])
+    return 0
+
+# 119433 too high
+
+
 with open(data_file) as f:
     for line in f.readlines():
-        line = line.strip()
+        print(dfs(eval(line.strip())))
         
+
+
+# part 1
+# # https://adventofcode.com/2023
+# import sys
+# import pathlib
+# import re
+
+# filepath = pathlib.Path(__file__)
+# sys.path.append(str(filepath.parent.parent))
+# from helpers import * 
+
+# filepath = pathlib.Path(__file__)
+# data_file = filepath.parent.joinpath(filepath.stem + '.dat')
+
+
+# with open(data_file) as f:
+#     for line in f.readlines():
+#         print(sum(map(int, re.findall(r'-?\d+', line.strip()))))
--- 2015/13.py
+++ 2015/13.py
@@ -1,6 +1,8 @@
 # https://adventofcode.com/2023
 import sys
 import pathlib
+import re
+import random
 
 filepath = pathlib.Path(__file__)
 sys.path.append(str(filepath.parent.parent))
@@ -9,7 +11,113 @@ from helpers import *
 filepath = pathlib.Path(__file__)
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
+
+m = {}
+people = set()
 with open(data_file) as f:
     for line in f.readlines():
         line = line.strip()
+        if line:
+            units = int(re.search(r'\d+', line)[0])
+            if 'lose' in line:
+                units *= -1
+
+            a = re.search(r'\w+', line)[0]
+            b = re.search(r'\w+', line[::-1])[0][::-1]
+            people.add(a)
+            people.add(b)
+            m[(a, b)] = units
+
+
+for p in people:
+    m[('me', p)] = 0
+    m[(p, 'me')] = 0
+
+people.add('me')
+
+def perms(index, collection, builder):
+    if index == len(collection):
+        yield builder
+        return
+    for ele in collection:
+        if ele not in builder:
+            builder.append(ele)
+            yield from perms(index + 1, collection, builder)
+            builder.pop()
+
+for k, v in m.items():
+    print(f'{k}: {v}')
+
+best = 0
+for perm in perms(0, list(people), []):
+    total = 0
+    for i in range(len(perm)):
+        if i == 0:
+            last = perm[-1]
+        else:
+            last = perm[i-1]
+        total += m[(perm[i], last)]
+        total += m[(last, perm[i])]
+    best = max(best, total) 
         
+print(best)
+
+
+
+# part 1
+# # https://adventofcode.com/2023
+# import sys
+# import pathlib
+# import re
+# import random
+
+# filepath = pathlib.Path(__file__)
+# sys.path.append(str(filepath.parent.parent))
+# from helpers import * 
+
+# filepath = pathlib.Path(__file__)
+# data_file = filepath.parent.joinpath(filepath.stem + '.dat')
+
+
+# m = {}
+# people = set()
+# with open(data_file) as f:
+#     for line in f.readlines():
+#         line = line.strip()
+#         if line:
+#             units = int(re.search(r'\d+', line)[0])
+#             if 'lose' in line:
+#                 units *= -1
+
+#             a = re.search(r'\w+', line)[0]
+#             b = re.search(r'\w+', line[::-1])[0][::-1]
+#             people.add(a)
+#             people.add(b)
+#             m[(a, b)] = units
+
+# def perms(index, collection, builder):
+#     if index == len(collection):
+#         yield builder
+#         return
+#     for ele in collection:
+#         if ele not in builder:
+#             builder.append(ele)
+#             yield from perms(index + 1, collection, builder)
+#             builder.pop()
+
+# for k, v in m.items():
+#     print(f'{k}: {v}')
+
+# best = 0
+# for perm in perms(0, list(people), []):
+#     total = 0
+#     for i in range(len(perm)):
+#         if i == 0:
+#             last = perm[-1]
+#         else:
+#             last = perm[i-1]
+#         total += m[(perm[i], last)]
+#         total += m[(last, perm[i])]
+#     best = max(best, total) 
+        
+# print(best)
