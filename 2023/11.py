2bac8d31f89fa987267d0e753c71aaa56d41310c
Andrew Duerig <duerig.andrew@gmail.com>
2023-12-11 00:35:26 -0500

    ok

--- 2023/11.py
+++ 2023/11.py
@@ -2,10 +2,10 @@
 import sys
 import pathlib
 
+filepath = pathlib.Path(__file__)
 sys.path.append(str(filepath.parent.parent))
 from helpers import * 
 
-filepath = pathlib.Path(__file__)
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
 
