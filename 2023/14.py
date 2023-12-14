ec2206cb18dd0241082fb9f9779f34d69186a1eb
Andrew Duerig <duerig.andrew@gmail.com>
2023-12-14 01:04:55 -0500

    ok

--- 2023/14.py
+++ 2023/14.py
@@ -15,69 +15,48 @@ with open(data_file) as f:
         if line:
             graph.append(list(line))
 
-def roll():
-    for y in range(len(graph)):
-        for x in range(len(graph[0])):
-            if graph[y][x] == '.':
-                new = y + 1
-                while new < len(graph):
-                    if graph[new][x] == '#':
-                        break
-                    if graph[new][x] == 'O':
-                        graph[new][x]= '.'
-                        graph[y][x]= 'O'
-                        break
-                    new += 1
-    # west
+
+def north_roll(graph):
     for x in range(len(graph[0])):
+        empty = 0
         for y in range(len(graph)):
-            if graph[y][x] == '.':
-                new = x + 1
-                while new < len(graph):
-                    if graph[y][new] == '#':
-                        break
-                    if graph[y][new] == 'O':
-                        graph[y][new]= '.'
-                        graph[y][x]= 'O'
-                        break
-                    new += 1
-    # south
-    for y in reversed(range(len(graph))):
-        for x in range(len(graph[0])):
-            if graph[y][x] == '.':
-                new = y - 1
-                while new > -1:
-                    if graph[new][x] == '#':
-                        break
-                    if graph[new][x] == 'O':
-                        graph[new][x]= '.'
-                        graph[y][x]= 'O'
-                        break
-                    new -= 1
-    # east
-    for x in reversed(range(len(graph[0]))):
-        for y in range(len(graph)):
-            if graph[y][x] == '.':
-                new = x - 1
-                while new > -1:
-                    if graph[y][new] == '#':
-                        break
-                    if graph[y][new] == 'O':
-                        graph[y][new]= '.'
-                        graph[y][x]= 'O'
-                        break
-                    new -= 1
+            if graph[y][x] == 'O':
+                graph[y][x] = '.'
+                graph[empty][x] = 'O'
+                empty += 1
+
+            elif graph[y][x] == '#':
+                empty = y + 1
+
+def rotate_right(graph):
+    return [[graph[y][x] for y in reversed(range(len(graph)))] for x in range(len(graph[0]))]
+
+
+def roll_all(graph):
+    north_roll(graph)
+    graph = rotate_right(graph)
+    north_roll(graph)
+    graph = rotate_right(graph)
+    north_roll(graph)
+    graph = rotate_right(graph)
+    north_roll(graph)
+    graph = rotate_right(graph)
+    return graph
+
 
 cache = {}
 i = 0
 while i < 1000000000:
-    roll()
+    graph = roll_all(graph)
     key = tuple([tuple(line) for line in graph])
     if key in cache:
         diff = i - cache[key]
         i += diff * ((1000000000 - i) // diff)
     cache[key] = i
     i += 1
+
+for line in graph:
+    print(''.join(line))
 
 total = 0
 for index, line in enumerate(graph):
