943c9cf8f2a52bc5d2ec2e5bfa8e9706a157eb57
Andrew Duerig <duerig.andrew@gmail.com>
2024-01-04 15:47:38 -0500

    auto

--- 2023/24_viz.py
+++ 2023/24_viz.py
@@ -105,20 +105,27 @@ def scale(min_n, max_n, n):
 
 
 
-
-def print_grid(t=0, axis='x'):
+def print_grid(t=0, axis='x', my_collider=None):
     if axis == 'x':
         dim = 0
-    else:
+    elif axis == 'z':
         dim = 2
 
     abs_mins = [float('inf'), float('inf'), float('inf')]
     abs_maxs = [-float('inf'), -float('inf'), -float('inf')]
 
-    for dim in [0, 1, 2]:
+    for sub_dim in [0, 1, 2]:
         for s in stones:
-            abs_mins[dim] = min(abs_mins[dim], s[0][dim])
-            abs_maxs[dim] = max(abs_maxs[dim], s[0][dim])
+            abs_mins[sub_dim] = min(abs_mins[sub_dim], s[0][sub_dim])
+            abs_maxs[sub_dim] = max(abs_maxs[sub_dim], s[0][sub_dim])
+
+    (cx_start, cy_start, cz_start), (cxvel, cyvel, czvel) = my_collider
+    cx = cxvel * t + cx_start
+    cy = cyvel * t + cy_start
+    cz = czvel * t + cz_start
+
+
+    cscales = [scale(abs_mins[0], abs_maxs[0], cx), (scale(abs_mins[1], abs_maxs[1], cy)), scale(abs_mins[2], abs_maxs[2], cz)]
 
     grid = [[[] for y in range(101)] for x in range(101)]
     for stone in stones:
@@ -136,6 +143,12 @@ def print_grid(t=0, axis='x'):
         if scales[1] > -1 and scales[1] < len(grid) and scales[dim] > -1 and scales[dim] < len(grid[0]):
             grid[scales[1]][scales[dim]].append(stone)
 
+    print(cscales)
+    if cscales[1] > -1 and cscales[1] < len(grid) and cscales[dim] > -1 and cscales[dim] < len(grid[0]):
+        grid[int(cscales[1])][int(cscales[dim])].insert(0, my_collider)
+        print('inserted at')
+
+
     for row in grid:
         to_print = []
         maxes = [-float('inf'), -float('inf'), -float('inf')]
@@ -144,38 +157,88 @@ def print_grid(t=0, axis='x'):
             if not col:
                 to_print.append('·')
             else:
-                first_stone = col[0]
-                xvel, yvel, zvel = first_stone[1]
+                relevant_vels = []
+                for (_, _, _), (xvel, yvel, zvel) in col:
+                    maxes[0] = max(maxes[0], xvel)
+                    maxes[1] = max(maxes[1], yvel)
+                    maxes[2] = max(maxes[2], zvel)
+                    mins[0] = min(mins[0], xvel)
+                    mins[1] = min(mins[1], yvel)
+                    mins[2] = min(mins[2], zvel)
 
-                maxes[0] = max(maxes[0], xvel)
-                maxes[1] = max(maxes[1], yvel)
-                maxes[2] = max(maxes[2], zvel)
-                mins[0] = min(mins[0], xvel)
-                mins[1] = min(mins[1], yvel)
-                mins[2] = min(mins[2], zvel)
+                    vel_dims = [xvel, yvel, zvel]
+                    relevant_vels.append(vel_dims[dim])
 
-                # char = red('^') if yvel < 0 else blue('v')
+                if my_collider in col:
+                    color = yellow
+                    char = '>' if relevant_vels[col.index(my_collider)] > 0 else '<'
+                else:
+                    char = ''
+                    positive = True if relevant_vels[0] > 0 else False
+                    for vel in relevant_vels:
+                        if positive:
+                            if vel < 0:
+                                char = '*'
+                                color = green
+                                break
+                            char = '>'
+                            color = blue
+                        else:
+                            if vel > 0:
+                                char = '*'
+                                color = green
+                                break
+                            char = '<'
+                            color = red
+                to_print.append(color(char))
 
-                char = red('<') if xvel < 0 else blue('>')
-                # char = red('<') if zvel < 0 else blue('>')
-
-                to_print.append(char)
         if maxes[dim] != -float('inf'):
             to_print.append(f'{maxes[dim]:>8}, {mins[dim]:>8}')
         print(''.join(to_print))
-    print(f'Showing y and {axis}, t = {t}')
+    print(f'Showing y and {axis}, t = {t}.')
+    print(f'x: {abs_mins[0]:,} - {abs_maxs[0]:,}, y: {abs_mins[1]:,} - {abs_maxs[1]:,}, z: {abs_mins[2]:,} - {abs_maxs[2]:,}')
+
+
+# things move meaningfully at ~1000000000 (1,000,000,000)
+# most have been hit by       ~759460080040 (759,460,080,040)
 
 t = 0
 dim = 'x'
 
-print_grid(t=t, axis=dim)
+my_collider = (
+    (
+        300000000000000, 
+        100000000000000,
+        255000000000000,
+    ), 
+    (
+        -12,
+        330,
+        70,
+    ),
+)
+# y def positive, z probably positive, x not sure, maybe negative
+
+last_change = 3000000000
+
+print_grid(t=t, axis=dim, my_collider=my_collider)
 while True:
     ok = input('')
     if ok in ['x', 'z']:
         dim = ok
-        print_grid(t=t, axis=dim)
+        print_grid(t=t, axis=dim, my_collider=my_collider)
+    elif ok.startswith ('c '):
+        x, y, z, xvel, yvel, zvel = ok[2:].strip().split()
+        my_collider = ((x, y, z), (xvel, yvel, zvel))
     elif ok.isnumeric():
         t = int(ok)
-        print_grid(t=t, axis=dim)
+        print_grid(t=t, axis=dim, my_collider=my_collider)
+    elif ok.startswith('+') or ok.startswith('-'):
+        last_change = int(ok[1:])
+        t += last_change
+        print_grid(t=t, axis=dim, my_collider=my_collider)
+    elif not ok.strip() and last_change is not None:
+        t += last_change
+        print_grid(t=t, axis=dim, my_collider=my_collider)
     else:
         print_red(f'didnt understand {ok}')
