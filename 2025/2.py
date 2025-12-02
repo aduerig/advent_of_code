0418361befe4e1ec9a43b2883992fefc589baf3f
Andrew Duerig <duerig.andrew@gmail.com>
2025-12-02 17:25:43 -0500

    auto

--- 2025/2.py
+++ 2025/2.py
@@ -12,4 +12,26 @@ data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 with open(data_file) as f:
     for line in f.readlines():
         line = line.strip()
-        
+
+
+def invalid(n):
+    s = str(n)
+    if n % 2 == 0:
+        half = len(s) // 2
+        a, b = s[:half], s[half:]
+        return a == b
+    return True
+
+total = 0
+ranges = line.split(',')
+for r in ranges:
+    first_id, last_id = map(int, r.split('-'))
+
+    for i in range(first_id, last_id + 1):
+        if invalid(i):
+            total += i
+
+print(total)
+
+# 1227775554
+# 651329497582753
