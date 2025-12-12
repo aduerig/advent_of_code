0ff46d404d27a4446c74c598a73aa0152a45791d
Andrew Duerig <gamechefproductions@gmail.com>
2025-12-12 16:11:15 -0500

    auto

--- 2025/10.py
+++ 2025/10.py
@@ -1,6 +1,9 @@
+# forgot to save part 1
+
 # https://adventofcode.com/2023
 import sys
 import pathlib
+from collections import deque
 
 filepath = pathlib.Path(__file__)
 sys.path.append(str(filepath.parent.parent))
@@ -35,5 +38,36 @@ with open(data_file) as f:
         machines.append(whole_machine)
 
 
+def solve_machine(needed_jolt, all_buttons):
+    start_state = [0] * len(needed_jolt)
+    queue = [[tuple(start_state), 0]]
+    queue = deque(queue)
+    seen = set()
+    print(f'{needed_jolt=}')
+    while queue:
+        curr_jolt, presses = queue.popleft()
+        if curr_jolt in seen:
+            continue
+        seen.add(curr_jolt)
+        if curr_jolt == needed_jolt:
+            return presses
+        
+        for to_go, needed in zip(curr_jolt, needed_jolt):
+            if to_go > needed:
+                continue
+
+        for b in all_buttons:
+            new_jolt = list(curr_jolt)
+            for to_flip in b:
+                new_jolt[to_flip] += 1
+            new_state = [tuple(new_jolt), presses + 1]
+            queue.append(new_state)
+
+        
+
+presses = 0
 for indic, all_buttons, jolts in machines:
     print(f'indic: {indic}, all_buttons: {all_buttons}, jolts: {jolts}')
+    presses += solve_machine(tuple(jolts), all_buttons)
+
+print(f'Needs {presses} presses')
